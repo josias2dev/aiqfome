0e4a2e3ade4fd1986e4041c592f088ce3dbf0605
josias <josiasr.ribeiro@gmail.com>
2021-06-28 11:40:11 -0300

    Update inject

--- inject/inject.js
+++ inject/inject.js
@@ -1,22 +1,37 @@
-const createButton = () => {
-
-}
-
-setTimeout(()=> {
- 
-  createButton()
 
 
-  const buttonSave = document.querySelector('.btn-salvar')
-  const buttonAttributes = buttonSave.attributes;
-  
-  let componentId = ""  
-  buttonAttributes.forEach(attribute => {
-    if(attribute.includes('data-v')){
-      componentId = attribute.replace('data-v-', '')
-    }
+const main = document.querySelector('.mogo main')
+
+const buttonSave = document.querySelector('.btn-salvar')
+const buttonAttributes = [...buttonSave.attributes];
+
+let componentId = ""
+buttonAttributes.forEach(({ name }) => {
+  if (name.includes('data-v')) {
+    componentId = name.replace('data-v-', '')
+  }
+})
+
+const categorias = __VUE_HOT_MAP__[componentId].instances[0].menu.cardapios_categorias
+categorias.forEach(categoria => {
+  const itens = categoria.itens
+
+  const categoriaWrapper = document.createElement('div')
+  categoriaWrapper.classList = "mogo-integration-categoria"
+  categoriaWrapper.innerHTML = `
+          <h1>${categoria.nome}</h1>
+          <div class="categoria-itens"></div>`
+
+  const itensWrapper = categoriaWrapper.querySelector('.categoria-itens')
+  itens.forEach(item => {
+    const itemWrapper = document.createElement('div')
+    itemWrapper.classList = "mogo-integration-item"
+    itemWrapper.innerHTML = `
+      <div class="mogo-integration-description">${item.nome}</div>
+      <input type="number" value="${item.codigo_interno}">`
+
+    itensWrapper.appendChild(itemWrapper)
   })
 
-  const categorias =  __VUE_HOT_MAP__['componentId'].instances[0].menu.cardapios_categorias
-
-}, 10000)
+  main.appendChild(categoriaWrapper)
+})
